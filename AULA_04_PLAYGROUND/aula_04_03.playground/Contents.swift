617753b1a8ad78e4eeaa3c78265a9cf62968459b
William Daniel <wdarchenemy@gmail.com>
2022-03-10 21:35:10 -0300

    adicionando arquivo aula_04_03 com conceito de colorLiteral e imageLiteral

---
+++ AULA_04_PLAYGROUND/aula_04_03.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let color = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 0.96)
+
+// usar #colorLiteral()
+let red: UIColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+
+let image = #imageLiteral(resourceName = "")
