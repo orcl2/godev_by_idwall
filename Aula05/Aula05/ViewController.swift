70d68c4e37b6eb08983feff01ac2127468fad6cd
William Daniel <wdarchenemy@gmail.com>
2022-03-11 18:54:16 -0300

    adicionando projeto aula_05

---
+++ Aula05/Aula05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aula05
+//
+//  Created by Idwall Go Dev 003 on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
